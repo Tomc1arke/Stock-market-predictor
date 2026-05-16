461a2a66ba677d027b2f3a857739ac39d0059107
Tom Clarke <tom.clarke@mail.dcu.ie>
2026-05-16 16:32:48 +0100

    Added dataset and further code to main.py

--- main.py
+++ main.py
@@ -11,7 +11,39 @@ from sklearn.preprocessing import MinMaxScaler
 data_source = 'kaggle'
 
 if data_source == 'alphavantage':
-    # AlphaVantage code here
+    api_key = 'P7PNHYK42DZEF1Z3'
+
+    # American Airlines stock market prices
+    ticker = "AAL"
+
+    # JSON file with all the stock market data for AAL from the last 20 years
+    url_string = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=%s&outputsize=full&apikey=%s"%(ticker,api_key)
+
+    # Save data to this file
+    file_to_save = 'stock_market_data-%s.csv'%ticker
+
+    # If you haven't already saved data,
+    # Go ahead and grab the data from the url
+    # And store date, low, high, volume, close, open values to a Pandas DataFrame
+    if not os.path.exists(file_to_save):
+        with urllib.request.urlopen(url_string) as url:
+            data = json.loads(url.read().decode())
+            # extract stock market data
+            data = data['Time Series (Daily)']
+            df = pd.DataFrame(columns=['Date','Low','High','Close','Open'])
+            for k,v in data.items():
+                date = dt.datetime.strptime(k, '%Y-%m-%d')
+                data_row = [date.date(),float(v['3. low']),float(v['2. high']),
+                            float(v['4. close']),float(v['1. open'])]
+                df.loc[-1,:] = data_row
+                df.index = df.index + 1
+        print('Data saved to : %s'%file_to_save)        
+        df.to_csv(file_to_save)
+
+    # If the data is already there, just load it from the CSV
+    else:
+        print('File already exists. Loading data from CSV')
+        df = pd.read_csv(file_to_save)
     pass
 
 else:
@@ -23,3 +55,24 @@ else:
 
     print('Loaded data from the Kaggle repository')
     print(df.head())
+
+# Sort DataFrame by date
+df = df.sort_values('Date')
+
+# Double check the result
+df.head()
+
+plt.figure(figsize = (18,9))
+plt.plot(range(df.shape[0]),(df['Low']+df['High'])/2.0)
+plt.xticks(range(0,df.shape[0],500),df['Date'].loc[::500],rotation=45)
+plt.xlabel('Date',fontsize=18)
+plt.ylabel('Mid Price',fontsize=18)
+plt.show()
+
+# First calculate the mid prices from the highest and lowest
+high_prices = df.loc[:, 'High'].values
+low_prices = df.loc[:, 'Low'].values
+mid_prices = (high_prices+low_prices)/2.0
+
+train_data = mid_prices[:11000]
+test_data = mid_prices[11000:]
